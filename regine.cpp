60651fa759e552f28d34c19d7bec3b5a407aa3c6
leonardstaniloiu <leonard.staniloiu@student.upt.ro>
2022-12-13 13:13:39 +0200

    update

--- regine.cpp
+++ regine.cpp
@@ -1,111 +1,62 @@
-/* C++ program to solve N Queen Problem using
-   backtracking */
+#include <iostream>
 
-#include <bits/stdc++.h>
-#define N 8
 using namespace std;
 
-/* A utility function to print solution */
-void printSolution(int board[N][N])
+
+int contorSolutii = 0;
+int n = 8;
+int regine[100];
+
+bool verificare(int aux)
 {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
-            cout << " " << board[i][j] << " ";
-        printf("\n");
-    }
-}
-
-/* A utility function to check if a queen can
-   be placed on board[row][col]. Note that this
-   function is called when "col" queens are
-   already placed in columns from 0 to col -1.
-   So we need to check only left side for
-   attacking queens */
-bool isSafe(int board[N][N], int row, int col)
-{
-    int i, j;
-
-    /* Check this row on left side */
-    for (i = 0; i < col; i++)
-        if (board[row][i])
-            return false;
-
-    /* Check upper diagonal on left side */
-    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-        if (board[i][j])
-            return false;
-
-    /* Check lower diagonal on left side */
-    for (i = row, j = col; j >= 0 && i < N; i++, j--)
-        if (board[i][j])
-            return false;
-
+    for (int i = 0; i < aux; ++i)
+        if(regine[i] == regine[aux] || aux-i == abs(regine[aux]-regine[i]))
+        return false;
     return true;
+
 }
 
-/* A recursive utility function to solve N
-   Queen problem */
-bool solveNQUtil(int board[N][N], int col)
+bool solutie(int aux)
 {
-    /* base case: If all queens are placed
-      then return true */
-    if (col >= N)
+    if(aux == n)
         return true;
-
-    /* Consider this column and try placing
-       this queen in all rows one by one */
-    for (int i = 0; i < N; i++) {
-        /* Check if the queen can be placed on
-          board[i][col] */
-        if (isSafe(board, i, col)) {
-            /* Place this queen in board[i][col] */
-            board[i][col] = 1;
-
-            /* recur to place rest of the queens */
-            if (solveNQUtil(board, col + 1))
-                return true;
-
-            /* If placing queen in board[i][col]
-               doesn't lead to a solution, then
-               remove queen from board[i][col] */
-            board[i][col] = 0; // BACKTRACK
-        }
-    }
-
-    /* If the queen cannot be placed in any row in
-        this column col  then return false */
     return false;
 }
 
-/* This function solves the N Queen problem using
-   Backtracking. It mainly uses solveNQUtil() to
-   solve the problem. It returns false if queens
-   cannot be placed, otherwise, return true and
-   prints placement of queens in the form of 1s.
-   Please note that there may be more than one
-   solutions, this function prints one  of the
-   feasible solutions.*/
-bool solveNQ()
+void afisareSolutii()
 {
-    int board[N][N] = { { 0, 0, 0, 0, 0, 0, 0, 0 },
-                        { 0, 0, 0, 0, 0, 0, 0, 0 },
-                        { 0, 0, 0, 0, 0, 0, 0, 0 },
-                        { 0, 0, 0, 0, 0, 0, 0, 0 } };
-
-    if (solveNQUtil(board, 0) == false) {
-        cout << "Solution does not exist";
-        return false;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if(regine[j] == i)
+                cout << "Q";
+            else
+                cout << "-";
+            cout << " ";
+        }
+        cout << endl;
     }
-
-    printSolution(board);
-    return true;
+    cout << endl;
+    contorSolutii++;
 }
 
-// driver program to test above function
+void backTracking(int coloana)
+{
+    for (int i = 0; i < coloana; ++i) {
+        regine[coloana] = i;
+        if(verificare(coloana))
+        {
+            if (solutie(coloana)) {
+                afisareSolutii();
+            } else
+                backTracking(coloana + 1);
+        }
+    }
+}
+
 int main()
 {
-    solveNQ();
+    backTracking(1);
+
+
     return 0;
 }
-
-// This code is contributed by Aditya Kumar (adityakumar129)
